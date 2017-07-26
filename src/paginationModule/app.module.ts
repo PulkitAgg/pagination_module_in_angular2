938fde91291bc031a7bcc86982a4122fd22ecbca
Pulkit Aggarwal <pulkitagg1010@gmail.com>
2017-07-26 10:25:02 +0530

    Update app.module.ts

--- src/paginationModule/app.module.ts
+++ src/paginationModule/app.module.ts
@@ -1,24 +1,12 @@
-// import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { HttpModule } from '@angular/http';
-import { CommonModule } from '@angular/common';
-// import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';;
 import { PaginationComponent } from './pagination/pagination.component';
-// import { TestComponent } from './test/test.component';
-// import { PaginationDirective } from './pagination.directive';
 
 @NgModule({
   declarations: [
-    // AppComponent,
     PaginationComponent
-    // TestComponent,
-    // PaginationDirective
   ],
   imports: [
-    // BrowserModule,
-    // FormsModule,
-    // HttpModule,
     CommonModule
   ],
   providers: [],
